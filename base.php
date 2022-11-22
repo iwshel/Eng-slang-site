1d76e50316c57278e24f0fbb3d3b8dd92d541f03
лизочка <2004lisa2004@gmail.com>
2022-11-22 21:20:35 +0300

    added php

---
+++ base.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="style2.css">
+<ul class="menu-main">
+        <li><a href="index.html" class="current">Словарь</a></li> 
+        <li><a href="cont2.html" class="">Контакты</a></li>
+      </ul>
