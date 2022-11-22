b2f55563759c5b940a6d5b209e7b748014d9c6a2
лизочка <2004lisa2004@gmail.com>
2022-11-22 21:20:58 +0300

    added search on bd

--- base.php
+++ base.php
@@ -3,3 +3,16 @@
         <li><a href="index.html" class="current">Словарь</a></li> 
         <li><a href="cont2.html" class="">Контакты</a></li>
       </ul>
+      
+<?php
+  $phrase = filter_var(trim($_POST['phrase']), FILTER_SANITIZE_STRING);
+
+
+  $mysql=new mysqli('localhost:3307', 'root', 'root', 'phrase-bd');
+  $result=$mysql->query("SELECT * FROM `phrases` WHERE `phrase`='$phrase'"); 
+  $mass = $result->fetch_assoc();
+  if(count($mass)==0) {
+    echo '<h1>Такое выражение не найдено</h1>';
+    echo '<a href="javascript: history.back ()" class="naz">&#8249; Вернуться к поиску</a>';
+    exit();
+  }?> <br>
